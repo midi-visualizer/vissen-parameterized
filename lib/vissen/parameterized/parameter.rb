9fc782448f8e29e5a2a2b289eedfd260bb1c7908
Sebastian Lindberg <seb.lindberg@gmail.com>
2018-05-10 12:15:49 +0200

    Added a parameter implementation

--- lib/vissen/parameterized/parameter.rb
+++ lib/vissen/parameterized/parameter.rb
@@ -2,8 +2,71 @@
 
 module Vissen
   module Parameterized
+    # Parameter respond to #value and can either return a locally stored
+    # constant or the value of a target object.
+    #
+    # === Usage
+    #
+    #   parameter = Parameter.new Value::Real
+    #   parameter.set 42
+    #   parameter.value # => 42
+    #
+    #   target = Value::Real.new 4.2
+    #   parameter.bind target
+    #   parameter.value # => 4.2
+    #
     class Parameter
       extend Forwardable
+
+      def_delegators :@target, :value, :tainted?, :untaint!
+
+      # @param  value_klass [Class] the value type supported by the parameter.
+      def initialize(value_klass)
+        @constant = value_klass.new
+        clear!
+      end
+
+      # Unbinds the parameter and resets the value of the to the default of the
+      #   value class.
+      #
+      # @return [self]
+      def clear!
+        set @constant.class::DEFAULT
+      end
+
+      # @return [false] if the parameter is bound to a value object.
+      # @reutrn [true] otherwise.
+      def constant?
+        @constant.equal? @target
+      end
+
+      # @raise  [RuntimeError] if the parameter is constant.
+      #
+      # @return [#value] the value target.
+      def target
+        raise RuntimeError if constant?
+        @target
+      end
+
+      # Writes a constant value to the parameter.
+      #
+      # @param  value [Object] the value to set.
+      # @return [self]
+      def set(value)
+        @constant.write value
+        @target = @constant
+        self
+      end
+
+      # TODO: validate the value type
+      #
+      # @param  obj [#value] the value object to bind to.
+      # @return [self]
+      def bind(obj)
+        raise TypeError unless obj.respond_to?(:value)
+        @target = obj
+        self
+      end
     end
   end
 end
---
+++ test/vissen/parameterized/parameter_test.rb
@@ -0,0 +1,113 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+describe Vissen::Parameterized::Parameter do
+  subject { Vissen::Parameterized::Parameter }
+
+  let(:value_klass) { Vissen::Parameterized::Value::Real }
+  let(:parameter)   { subject.new value_klass }
+  let(:target)      { value_klass.new 42 }
+
+  describe '.new' do
+    it 'creates a constant parameter' do
+      assert parameter.constant?
+    end
+
+    it 'uses the default value of the given type' do
+      assert_equal value_klass::DEFAULT, parameter.value
+    end
+
+    it 'is initially tainted' do
+      assert parameter.tainted?
+    end
+  end
+
+  describe 'constant parameter' do
+    describe '#clear!' do
+      it 'resets the constant value' do
+        parameter.set 42
+        parameter.clear!
+        assert_equal value_klass::DEFAULT, parameter.value
+      end
+    end
+
+    describe '#set' do
+      it 'stores a new constant value' do
+        parameter.set 42
+        assert_equal 42.0, parameter.value
+      end
+    end
+
+    describe '#tainted?' do
+      it 'returns the state of the constant value' do
+        assert parameter.tainted?
+        parameter.untaint!
+        refute parameter.tainted?
+      end
+    end
+
+    describe '#target' do
+      it 'raises a RuntimeError' do
+        assert_raises(RuntimeError) { parameter.target }
+      end
+    end
+  end
+
+  describe '#bind' do
+    it 'returns self' do
+      assert_equal parameter, parameter.bind(target)
+    end
+  end
+
+  describe 'bound parameter' do
+    before { parameter.bind target }
+
+    describe '#constant?' do
+      it 'returns false' do
+        refute parameter.constant?
+      end
+    end
+
+    describe '#clear!' do
+      before { parameter.clear! }
+
+      it 'makes the parameter constant' do
+        assert parameter.constant?
+      end
+
+      it 'sets the value to the default' do
+        assert_equal value_klass::DEFAULT, parameter.value
+      end
+    end
+
+    describe '#target' do
+      it 'returns the target' do
+        assert_same target, parameter.target
+      end
+    end
+
+    describe '#set' do
+      it 'stores a new constant value' do
+        parameter.set 4.2
+        assert parameter.constant?
+        assert_equal 4.2, parameter.value
+      end
+    end
+
+    describe '#tainted?' do
+      it 'returns the state of the target' do
+        assert parameter.tainted?
+        parameter.untaint!
+        refute parameter.tainted?
+        refute target.tainted?
+      end
+    end
+
+    describe '#value' do
+      it 'returns the value of the target' do
+        assert_equal target.value, parameter.value
+      end
+    end
+  end
+end
